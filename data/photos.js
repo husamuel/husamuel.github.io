ca4e1e19f9aecd0cd15ad0a832aea8221c316b94
husamuel <hugoleonor10@gmail.com>
2026-01-14 17:31:16 +0000

    photos

--- data/photos.js
+++ data/photos.js
@@ -1,4 +1,14 @@
 const photos = [
-  { src: "photos/d018c04a-a1d1-46b4-9d20-2bd25e072979.jpeg", alt: "First at the Louvre", caption: "First at the Louvre", date: "2026-01-14" },
-  { src: "photos/f35d1ee9-f82b-4722-b577-5f29353f22a7.jpeg", alt: "My favourite painting until now", caption: "My favourite painting until now", date: "2026-01-14" },
+  {
+    src: "/photos/d018c04a-a1d1-46b4-9d20-2bd25e072979.jpeg",
+    alt: "First at the Louvre",
+    caption: "First at the Louvre",
+    date: "2026-01-14"
+  },
+  {
+    src: "/photos/f35d1ee9-f82b-4722-b577-5f29353f22a7.jpeg",
+    alt: "My favourite painting until now",
+    caption: "My favourite painting until now",
+    date: "2026-01-14"
+  }
 ];
