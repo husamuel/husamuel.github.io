c0f17b09f27546c15b477f87137559064add81dc
husamuel <hugoleonor10@gmail.com>
2026-01-21 19:01:54 +0000

    reset

--- data/photos.js
+++ data/photos.js
@@ -1,14 +1,9 @@
 const photos = [
   {
-    src: "/photos/d018c04a-a1d1-46b4-9d20-2bd25e072979.jpeg",
-    alt: "First at the Louvre",
-    caption: "First at the Louvre",
+    src: "",
+    alt: "",
+    caption: "",
     date: "2026-01-14"
   },
-  {
-    src: "/photos/f35d1ee9-f82b-4722-b577-5f29353f22a7.jpeg",
-    alt: "My favourite painting until now",
-    caption: "My favourite painting until now",
-    date: "2026-01-14"
-  }
 ];
+qchat
--- data/thoughts.js
+++ data/thoughts.js
@@ -1,7 +1,7 @@
 const thoughts = [
     {
         date: "2026-01-14",
-        content: "Self-esteem is the relationship each person has with themselves; if you often break your own promises, it's likely you have low self-esteem."
+        content: ""
     },
 
 ];
